eead34bf63492dd7c882df77808c011e79d203d6
tr1viall <666d.aks666@gmail.com>
2025-10-22 14:44:19 +0500

    dobavil html

--- proekSERV/SAMserv/models.py
+++ proekSERV/SAMserv/models.py
@@ -5,7 +5,7 @@ class Client(models.Model):
     email = models.CharField(max_length=100)
     last_name = models.CharField(max_length=25)
     first_name = models.CharField(max_length=25)
-    middle_name = models.CharField(max_length=25, blank=True, null=True)  # ← теперь опционально
+    middle_name = models.CharField(max_length=25, blank=True, null=True)  # ← опционально
 
     def __str__(self):
         return f"{self.last_name} {self.first_name}"
